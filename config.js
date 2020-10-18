0a343a4256907457183a10303f414329d5355e64
Stef361 <73083091+Stef361@users.noreply.github.com>
2020-10-19 02:17:16 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     "ownerID": 339446545785159680
     "admins": [""],
     "support": [""],
-    "token": "", NzY3NTIxNDc4MDAxNDI2NDMz.X4zIFA.91bfcQqWzKCA0VHePyk6wPvS4P0
+    "token": "", NzY3NTIxNDc4MDAxNDI2NDMz.X4zIFA.FaxSohMvooE78dRifevJgSno8Cg
     "dashboard" : {
       "oauthSecret": "", LduTHl0uX4KCe0kXMv-l-Hxb-jVnBSAN
       "callbackURL": `https://dash.vortexbot.org/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
